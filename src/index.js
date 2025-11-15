d60337f142eb5714958f27e46e312c50b2d474e2
github_christiandc19 <christiandc19@hotmail.com>
2025-11-15 13:44:13 -0800

    styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ChatBox from "./ChatBox";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
