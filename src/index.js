1a48f7b513f90c53a244a2fa0ca6f44701b726ab
github_christiandc19 <christiandc19@hotmail.com>
2025-11-15 13:46:56 -0800

    styling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import ChatBox from "./ChatBox";
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 🔥 Expose globally so WordPress can access
+window.ChatBox = ChatBox;
+window.React = React;
+window.ReactDOM = ReactDOM;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ❗ Do NOT render anything here.
+// WordPress will handle mounting using ReactDOM.createRoot()
+// inside your footer snippet.
