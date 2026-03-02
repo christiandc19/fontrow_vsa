4a5b4207976f4d353f75181ece443a13fbed1880
github_christiandc19 <christiandc19@hotmail.com>
2026-03-01 16:54:42 -0800

    modified App.js. Updated the chatbox to Font Row

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import ChatBox from "./components/ChatBox/ChatBox";
 
-import itanexConfig from "./chatbots/itanex/config";
+import fontrowConfig from "./chatbots/fontrow/config";
 // import fontrowConfig from "./chatbots/fontrow/config";
 
 export default function App() {
   return (
     <div className="App">
-      <ChatBox config={itanexConfig} />
+      <ChatBox config={fontrowConfig} />
     </div>
   );
 }
