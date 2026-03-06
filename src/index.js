9563cc9099f1c9755023bae056d9e31fdbfd8d2f
jcabatac <jcabatac@doehle-mnl.com>
2026-03-06 11:41:02 +0800

    chatbot | update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
+// This line of code is for local development and testing. It renders the App component into the root element of the HTML document. In production, the FontRowChatbot function will be used to render the ChatBox component instead.
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+// This line of code is for production. It defines a global function FontRowChatbot that takes a configuration object as an argument. It creates a new div element, appends it to the body of the document, and renders the ChatBox component into that div using ReactDOM.createRoot. The ChatBox component receives the configuration object as a prop.
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ChatBox from "./components/ChatBox/ChatBox";
 import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ 
+window.FontRowChatbot = function (config = {}) {
+  const container = document.createElement("div");
+  container.id = "fontrow-chatbot-root";
+  document.body.appendChild(container);
+ 
+  const root = ReactDOM.createRoot(container);
+ 
+  root.render(
+<ChatBox config={config} />
+  );
+};
