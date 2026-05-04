252b519785939f218831727188d73c40a39fb1d3
github_christiandc19 <christiandc19@hotmail.com>
2026-05-04 14:09:53 -0700

    created new widget version

--- src/chatbots/registry.js
+++ src/chatbots/registry.js
@@ -1,11 +1,14 @@
 import fontrowConfig from "./clients/fontrow";
 import robinRunConfig from "./clients/robinRun";
+import wsaConfig from "./clients/wsa";
 import evergreenHeightsConfig from "./clients/evergreenHeights"; // 👈 ADD
 
 const chatbotRegistry = {
   fontrow: fontrowConfig,
   "robin-run": robinRunConfig,
   "evergreen-heights": evergreenHeightsConfig, // 👈 ADD
+  "web-smart-assistant": wsaConfig, // ✅ ADD THIS
+
 };
 
 export function getClientConfig(clientKey) {
