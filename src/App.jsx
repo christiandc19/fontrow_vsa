f5be768e8c19245f331f7e38b7454e2b4aeb8390
github_christiandc19 <christiandc19@hotmail.com>
2026-05-12 12:18:55 -0700

    configure vite config file for widget naming

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 
 import ChatBox from "./components/ChatBox/ChatBox.jsx";
-import webSmartAssistant from "./chatbots/clients/wsa.js";
+// import webSmartAssistant from "./chatbots/clients/wsa.js";
+import evergreenHeights from "./chatbots/clients/evergreenHeights.js";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <ChatBox config={webSmartAssistant} />
+        {/* <ChatBox config={webSmartAssistant} /> */}
+        <ChatBox config={evergreenHeights} />
       </div>
     </BrowserRouter>
   );
